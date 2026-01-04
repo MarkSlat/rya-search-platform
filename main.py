d094ef1346a961a165d72b2ecefda1e5dd53d428
Mark <100161972+MarkSlat@users.noreply.github.com>
2026-01-04 17:29:30 +0000

    updates

--- main.py
+++ main.py
@@ -21,13 +21,14 @@ def populateAirportsAndFlights(driver, airports, flights):
 
 driver = get_neo4j_driver()
 
-# GraphRepository(driver).clearGraph()
+# # GraphRepository(driver).clearGraph()
 
-airports = getActiveAirports()
+# airports = getActiveAirports()
 
-# baseAirports = ["DUB", "SNN", "NOC"]
-# baseAirports = ["SNN", "NOC"]
-baseAirports = ["SNN"]
+# # baseAirports = ["DUB", "SNN", "NOC"]
+# # baseAirports = ["SNN", "NOC"]
+# # baseAirports = ["SNN"]
+# baseAirports = ["DUB", "SNN"]
 
 # desinations = set() 
 
@@ -47,25 +48,36 @@ baseAirports = ["SNN"]
 
 # GraphRepository(driver).save_distances(distances)
 
-origin_airports = ['SNN', 'DUB', 'NOC']
+origin_airports = ['SNN']
 
+# r1_dates = [
+#     date(2026, 2, 6),  # First Friday in February
+#     date(2026, 2, 13), # Second Friday in February
+#     date(2026, 2, 20), # Third Friday in February
+#     date(2026, 2, 27)  # Fourth Friday in February
+# ]
+# r2_dates = [
+#     date(2026, 2, 8),  # First Sunday in February
+#     date(2026, 2, 15), # Second Sunday in February
+#     date(2026, 2, 22), # Third Sunday in February
+#     date(2026, 3, 1)  # Fourth Sunday in February
+# ]\
+    
 r1_dates = [
-    date(2026, 2, 6),  # First Friday in February
-    date(2026, 2, 13), # Second Friday in February
-    date(2026, 2, 20), # Third Friday in February
-    date(2026, 2, 27)  # Fourth Friday in February
+    date(2026, 3, 25)
 ]
 r2_dates = [
-    date(2026, 2, 8),  # First Sunday in February
-    date(2026, 2, 15), # Second Sunday in February
-    date(2026, 2, 22), # Third Sunday in February
-    date(2026, 3, 1)  # Fourth Sunday in February
-]
-lengths_of_stay = [2, 3, 4]  # Length of stay in days (e.g., 3 days, 4 days)
-blacklist_countries = ['Ireland', 'United Kingdom']  # Exclude destinations in Ireland and Spain
+    date(2026, 3, 29)
+]    
+    
+    
+lengths_of_stay = [2, 3, 4, 5, 6]  # Length of stay in days (e.g., 3 days, 4 days)
+blacklist_countries = ['Ireland', 'Poland', 'United Kingdom']  # Exclude destinations in Ireland and Spain
 
 # Run the query
-result = GraphRepository(driver).query_flights(origin_airports, r1_dates, r2_dates, lengths_of_stay, blacklist_countries)
+result = GraphRepository(driver).query_flights(origin_airports, r1_dates, r2_dates, lengths_of_stay, blacklist_countries, 100)
+
+print(len(result))
 
 # Print the result
 for row in result:
--- src/graphRepository.py
+++ src/graphRepository.py
@@ -64,30 +64,76 @@ class GraphRepository:
                 )
                 
                 
-    def query_flights(self, origin_airports, r1_dates, r2_dates, lengths_of_stay, blacklist_countries):
-        # Construct the Cypher query with dynamic parameters for lists of dates and lengths of stay
-        query = """
-        MATCH path = (o1:Airport)-[r1:FLYS_TO]->(d:Airport)-[r2:FLYS_TO]->(o2:Airport)
+    def query_flights(
+        self,
+        origin_airports,
+        r1_dates,
+        r2_dates,
+        lengths_of_stay,
+        blacklist_countries,
+        max_distance=None
+    ):
+        base_query = """
+        MATCH (o1:Airport)-[r1:FLYS_TO]->(d:Airport)-[r2:FLYS_TO]->(o2:Airport)
         WHERE o1.code IN $origin_airports
             AND r1.date IN $r1_dates
             AND r2.date IN $r2_dates
             AND r2.date >= r1.date
-            AND duration.between(r1.date, r2.date).days IN $lengths_of_stay  // Calculate difference in days between r1 and r2
-            AND NOT d.countryName IN $blacklist_countries  // Exclude destinations in the blacklist countries
+            AND duration.between(r1.date, r2.date).days IN $lengths_of_stay
+            AND NOT d.countryName IN $blacklist_countries
             AND o2.code IN $origin_airports
-        RETURN o1.code, r1.date, d.code, r2.date, o2.code
+        RETURN
+            o1.code AS origin,
+            r1.date AS outboundDate,
+            d.code  AS destination,
+            r2.date AS returnDate,
+            o2.code AS returnOrigin,
+            null    AS landAirport,
+            null    AS landDistance
         """
 
-        
-        # Prepare the query parameters
+        distance_query = """
+        MATCH (o1:Airport)-[r1:FLYS_TO]->(d1:Airport)
+            -[t:DISTANCE_TO]->(d2:Airport)
+            -[r2:FLYS_TO]->(o2:Airport)
+        WHERE o1.code IN $origin_airports
+            AND r1.date IN $r1_dates
+            AND r2.date IN $r2_dates
+            AND r2.date >= r1.date
+            AND duration.between(r1.date, r2.date).days IN $lengths_of_stay
+            AND NOT d1.countryName IN $blacklist_countries
+            AND NOT d2.countryName IN $blacklist_countries
+            AND o2.code IN $origin_airports
+            AND t.distance < $max_distance
+        RETURN
+            o1.code AS origin,
+            r1.date AS outboundDate,
+            d1.code AS destination,
+            r2.date AS returnDate,
+            o2.code AS returnOrigin,
+            d2.code AS landAirport,
+            t.distance AS landDistance
+        """
+
+        if max_distance is not None:
+            query = f"""
+            {base_query}
+            UNION
+            {distance_query}
+            """
+        else:
+            query = base_query
+
         params = {
             "origin_airports": origin_airports,
-            "r1_dates": r1_dates,  # List of specific dates for r1
-            "r2_dates": r2_dates,  # List of specific dates for r2
-            "lengths_of_stay": lengths_of_stay,  # List of lengths of stay in days
-            "blacklist_countries": blacklist_countries  # List of countries to blacklist
+            "r1_dates": r1_dates,
+            "r2_dates": r2_dates,
+            "lengths_of_stay": lengths_of_stay,
+            "blacklist_countries": blacklist_countries,
+            "max_distance": max_distance
         }
-        
+
         with self.driver.session() as session:
             result = session.run(query, params)
             return [record.data() for record in result]
+
